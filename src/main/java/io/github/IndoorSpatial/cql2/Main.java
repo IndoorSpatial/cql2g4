5dff7c4ac8465d368e53f01f8238a7c81641c319
Kunlin Yu <yukunlin@syriusrobotics.com>
2025-11-12 13:57:56 +0800

    Add some comment in Main
    Signed-off-by: Kunlin Yu <yukunlin@syriusrobotics.com>

--- src/main/java/io/github/IndoorSpatial/cql2/Main.java
+++ src/main/java/io/github/IndoorSpatial/cql2/Main.java
@@ -3,35 +3,41 @@ package io.github.IndoorSpatial.cql2;
 import io.github.IndoorSpatial.cql2.ast.AstNode;
 import io.github.IndoorSpatial.cql2.converter.sql.PropertyToQueryable;
 import io.github.IndoorSpatial.cql2.converter.sql.Queryable;
-import io.github.IndoorSpatial.cql2.converter.sql.QueryableType;
 import io.github.IndoorSpatial.cql2.converter.sql.SqlType;
-import com.fasterxml.jackson.core.JsonProcessingException;
 
 import java.util.HashMap;
 
 public class Main {
-    public static void main(String[] args) throws JsonProcessingException {
-        String cqlText = "speed > 3 AND S_CONTAINS(POLYGON ((0 0, 1 0, 1 1, 0 1, 0 0)), location)";
-        System.out.println("origin cql text: " + cqlText);
+    public static void main(String[] args) {
+        try {
+            // Example 1: Basic conversion - CQL2 text to AST, JSON, and SQL
+            String cqlText = "speed > 3 AND S_CONTAINS(POLYGON ((0 0, 1 0, 1 1, 0 1, 0 0)), location)";
+            System.out.println("Original CQL2 text: " + cqlText);
 
-        Cql2G4 cql2G4 = new Cql2G4();
-        AstNode astNode = cql2G4.textToAst(cqlText);
-        System.out.println("convert to ast:\n" + astNode.ToString());
-        System.out.println("convert to json: " + cql2G4.astToJsonString(astNode));
-        System.out.println("convert to sql: " + cql2G4.astToSql(astNode));
+            Cql2G4 cql2G4 = new Cql2G4();
+            AstNode astNode = cql2G4.textToAst(cqlText);
+            System.out.println("Convert to AST:\n" + astNode.ToString());
+            System.out.println("Convert to JSON: " + cql2G4.astToJsonString(astNode));
+            System.out.println("Convert to SQL: " + cql2G4.astToSql(astNode));
 
-        String cqlWithPropertyName = "speed > 1.2 AND \"obj.key\" = 'value1' AND A_CONTAINS(obj.binlocations, ('A-1-2-01-a'))";
-        System.out.println("another cql text: " + cqlWithPropertyName);
-        AstNode astNode2 = cql2G4.textToAst(cqlWithPropertyName);
-        System.out.println("convert it to sql directly:\n" + cql2G4.astToSql(astNode2));
+            // Example 2: Property mapping - map CQL2 properties to database queryable fields
+            String cqlWithPropertyName = "speed > 1.2 AND \"obj.key\" = 'value1' AND A_CONTAINS(obj.binlocations, ('A-1-2-01-a'))";
+            System.out.println("Another CQL2 text: " + cqlWithPropertyName);
+            AstNode astNode2 = cql2G4.textToAst(cqlWithPropertyName);
+            System.out.println("Convert to SQL directly:\n" + cql2G4.astToSql(astNode2));
 
-        HashMap<String, Queryable> queryables = new HashMap<>() {{
-            put("speed", new Queryable("speed", null, SqlType.Float));
-            put("obj.key", new Queryable("properties", "$.obj.key", SqlType.Text));
-            put("obj.binlocations", new Queryable("properties", "$.obj.binlocations[*]", SqlType.TextArray));
-        }};
-        PropertyToQueryable propertyToQueryable = new PropertyToQueryable(queryables);
-        Cql2G4 cql2G4WithQueryable = new Cql2G4(propertyToQueryable::toQueryable);
-        System.out.println("convert it to sql according to queryables:\n" +  cql2G4WithQueryable.astToSql(astNode2));
+            // Define property mappings: CQL2 property name -> database field/JSON path
+            HashMap<String, Queryable> queryables = new HashMap<>() {{
+                put("speed", new Queryable("speed", null, SqlType.Float));
+                put("obj.key", new Queryable("properties", "$.obj.key", SqlType.Text));
+                put("obj.binlocations", new Queryable("properties", "$.obj.binlocations[*]", SqlType.TextArray));
+            }};
+            PropertyToQueryable propertyToQueryable = new PropertyToQueryable(queryables);
+            Cql2G4 cql2G4WithQueryable = new Cql2G4(propertyToQueryable::toQueryable);
+            System.out.println("Convert to SQL according to queryables:\n" + cql2G4WithQueryable.astToSql(astNode2));
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
